858d3de52c3e864b857b809a02326ca938245a49
smilemcm@gmail.com <smilemcm@gmail.com>
2020-08-02 14:57:30 +0900

    음성분해하는 소스

--- test_play/storeVideo.py
+++ test_play/storeVideo.py
@@ -1,10 +1,12 @@
 import numpy as np
 import cv2
+import pytesseract
 
 
+(major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
 
 shotsPath = './out/%d.avi'
-cap = cv2.VideoCapture("Lecture3")
+cap = cv2.VideoCapture("goodmorning.mp4")
 fps = int(cap.get(cv2.CAP_PROP_FPS))
 
 size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
@@ -13,16 +15,33 @@ fourcc = int(cv2.VideoWriter_fourcc('X', 'V', 'I', 'D'))  # XVID codecs
 segRange = [(0, fps*60), (fps*60+1, fps*60*2), (fps*60*2+1, fps*60*3)]  # a list of starting/ending frame indices pairs
 
 
-for idx, (begFidx, endFidx) in enumerate(segRange):
-    writer = cv2.VideoWriter(shotsPath%idx, fourcc, fps, size)
-    cap.set(cv2.CAP_PROP_POS_FRAMES, begFidx)
-    ret = True  # has frame returned
+ret, frame = cap.read()
+prev_text = pytesseract.image_to_string(frame, lang='kor+eng')
+frame_number = cap.get(cv2.CAP_PROP_POS_FRAMES)
+
+while ret:
+    writer = cv2.VideoWriter(shotsPath%(frame_number+1), fourcc, fps, size)
+    cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number+1)
+
+    print(prev_text)
+
+
     while (cap.isOpened() and ret and writer.isOpened()):
         ret, frame = cap.read()
-        frame_number = cap.get(cv2.CAP_PROP_POS_FRAMES) - 1
-        if frame_number < endFidx:
+        cv2.imshow('output', frame)
+
+        if(cv2.waitKey(1) & 0xFF == ord('q')):
+            break
+        text = pytesseract.image_to_string(frame, lang='eng')
+
+        frame_number = cap.get(cv2.CAP_PROP_POS_FRAMES)
+        if text == prev_text:
             writer.write(frame)
         else:
+            prev_text = text
+            prev_frame_number = frame_number
             break
+
     writer.release()
 
+cv2.destroyWindow()
